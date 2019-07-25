0034e2c5f988e1fed0b87514b474b6874566cf71
itsdf07 <sdf0707>
2019-07-25 15:14:38 +0800

    增加用户登录成功/失败的界面跳转

--- src/main/java/com/aso/itsdf07/controller/UserController.java
+++ src/main/java/com/aso/itsdf07/controller/UserController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @Description: TODO
@@ -26,7 +23,7 @@ public class UserController {
     private UserService userService;
 
     /**
-     * 跳转到用户登录页面
+     * 跳转到用户登录页面:http://localhost:8080/user/login
      *
      * @return 登录页面
      */
@@ -38,30 +35,35 @@ public class UserController {
     /**
      * 获取用户名与密码，用户登录
      *
-     * @param request
-     * @param username
-     * @param password
      * @return
      */
-    @ResponseBody
     @RequestMapping(value = {"/userLogin"})
-    private String onUserLogin(HttpServletRequest request,
-                               @RequestParam("username") String username,
-                               @RequestParam("password") String password) {
+    private ModelAndView onUserLogin(UserEntity reqData) {
+        ModelAndView modelAndView = new ModelAndView();
+        String username = reqData.getUsername();
+        String password = reqData.getPasswd();
         if (StringUtils.isEmpty(username)) {
-            return "用户名不能为空";
+            modelAndView.setViewName("loginHtml");
+            modelAndView.addObject("msg", "账号不能为空");
+            return modelAndView;
         }
 
         if (StringUtils.isEmpty(password)) {
-            return "密码不能为空";
+            modelAndView.setViewName("loginHtml");
+            modelAndView.addObject("msg", "密码不能为空");
+            return modelAndView;
         }
 
         UserEntity user = userService.onUserLogin(username, password);
 
-        if (user != null) {                                                  //登录成功
-            request.getSession().setAttribute("session_user", user);     //将用户信息放入session  用于后续的拦截器
-            return "登录成功";
+        if (user != null) {
+            modelAndView.setViewName("mainHtml");
+            modelAndView.addObject("msg", "登陆成功");//登录成功
+//            request.getSession().setAttribute("session_user", user);     //将用户信息放入session  用于后续的拦截器
+            return modelAndView;
         }
-        return "登录失败，用户名或密码错误";
+        modelAndView.setViewName("loginHtml");
+        modelAndView.addObject("msg", "登录失败，用户名或密码错误");
+        return modelAndView;
     }
 }
